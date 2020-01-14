eebd6b6d21ef1ad6df19486f6f49a0b14f02188a
William Jelvin <william.jelvin@gmail.com>
2020-01-14 17:04:51 +0100

    added barebones redux store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './runtimeLogo.PNG';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
+
+  const { text } = props;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I fetched this --> {text} from the Redux store.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    text: state.someTextToRemove
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const initState = {
+    /**Remove this when we want to start using redux */
+    someTextToRemove: '*William*'
+}
+
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
