69a69cb6bba8d7d7500dac63edde46216caecaf8
William Jelvin <william.jelvin@gmail.com>
2020-01-23 11:17:56 +0100

    added delete functionality and button

---
+++ src/components/UI/DeleteButton/DeleteButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+      backgroundColor: '#EE4319'
+    },
+  }));
+
+function DeleteButton(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Button
+        variant="contained"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+        size='small'
+        onClick={() => props.click(props.index)}
+      >
+        Delete
+      </Button>
+    </div>
+  );
+}
+
+export default DeleteButton
